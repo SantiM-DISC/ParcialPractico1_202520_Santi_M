f12447e318def6728ade5fece2e757ffbf4bc99a
SantiM-DISC <s.montese@uniandes.edu.co>
2025-09-17 06:57:23 -0500

    Entidades

--- src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/PlanetaEntity.java
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/PlanetaEntity.java
@@ -1,5 +1,21 @@
 package co.edu.uniandes.dse.ParcialPractico1_202520.entities;
 
-public class PlanetaEntity {
+import jakarta.persistence.Entity;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.ManyToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Data
+@Entity
+public class PlanetaEntity extends BaseEntity {
     private String nombre;
+    private int poblacion;
+    private double diametro;
+
+    @PodamExclude
+    @OneToOne
+    private SistemaSolar sistemaSolar;
 }
--- src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/SistemaSolar.java
+++ src/main/java/co/edu/uniandes/dse/ParcialPractico1_202520/entities/SistemaSolar.java
@@ -1,7 +1,22 @@
 package co.edu.uniandes.dse.ParcialPractico1_202520.entities;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.ManyToMany;
+import uk.co.jemos.podam.common.PodamExclude;
+
+@Data
+@Entity
 public class SistemaSolar extends BaseEntity {
 
     private String nombre;
+    private String galaxia;
+    private int ratio;
+    private int n_stormtroopers;
+    @PodamExclude
+    @OneToMany(mappedBy = "planetas")
+    private List<PlanetaEntity> planetas = new ArrayList<>();
 
 }
